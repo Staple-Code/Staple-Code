00e5ece5112a90b392533a77388abb7e30137689
Hans Heeling <hansheeling@adaptivcomputing.com>
2015-03-16 20:44:11 -0600

    Corrected issue with method for loading form view adapter

--- library/Staple/Form/Form.class.php
+++ library/Staple/Form/Form.class.php
@@ -176,11 +176,9 @@ class Form
 		 */
 		if(Config::getValue('forms','elementViewAdapter') != '')
 		{
-			if(class_exists(Config::getValue('forms','elementViewAdapter')))
-			{
-				$this->setElementViewAdapter(Config::getValue('forms','elementViewAdapter'));
-			}
+			$this->setElementViewAdapter(Config::getValue('forms','elementViewAdapter'));
 		}
+
 
 		//Repopulate data from the session -- I might add this.....
 		//if($this->wasSubmitted())
@@ -760,7 +758,10 @@ JS;
 	 */
 	public function setElementViewAdapter($elementViewAdapter)
 	{
-		$this->elementViewAdapter = new $elementViewAdapter;
+		if(class_exists(Config::getValue('forms','elementViewAdapter')))
+		{
+			$this->elementViewAdapter = new $elementViewAdapter;
+		}
 		return $this;
 	}
 
