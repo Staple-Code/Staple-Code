941cfcb51e90235b8a223bbce60c730b77c1809a
Adam Day <advation@gmail.com>
2015-03-24 21:52:19 -0600

    Cleaned up DOM structure for the following Foundation form elements:
    TextElement
    TextareaElement
    PasswordElement
    HiddenElement
    SelectElement
    CheckboxElement
    CheckboxgroupElement
    RadioElement

--- application/forms/testForm.php
+++ application/forms/testForm.php
@@ -1,6 +1,11 @@
 <?php
+use Staple\Form\CheckboxElement;
+use Staple\Form\CheckboxGroupElement;
 use Staple\Form\Form;
+use Staple\Form\HiddenElement;
 use Staple\Form\PasswordElement;
+use Staple\Form\RadioElement;
+use Staple\Form\SelectElement;
 use Staple\Form\SubmitElement;
 use Staple\Form\TextareaElement;
 use Staple\Form\TextElement;
@@ -44,14 +49,45 @@ class testForm extends Form
         $password->setRequired();
 
         $textarea = new TextareaElement('textarea','Textarea Element');
-        $textarea->addAttrib('style','height:200px;')
-            ->setRequired()
-            ->addValidator(new LengthValidator(0,1000))
-            ->addAttrib('placeholder','1000 character limit')
-            ->setInstructions("Here is the test for the instructions");
+        $textarea->addAttrib('style','height:200px;');
+
+        $select = new SelectElement('select','Select Element');
+        $select->addOptionsArray(
+            array(
+                "1"=>"Option 1",
+                "2"=>"Option 2",
+                "3"=>"Option 3",
+                "4"=>"Option 4",
+                "5"=>"Option 5"
+            )
+        );
+
+        $hidden = new HiddenElement('hidden','Hidden Element');
+
+        $checkbox = new CheckboxElement('1','Single Checkbox Element');
+
+        $checkboxGroup = new CheckboxGroupElement('checkboxGroup','Checkbox Group Element');
+        $checkboxGroup->addCheckboxArray(
+            array(new CheckboxElement('1','One'),
+                new CheckboxElement('2','Two'),
+                new CheckboxElement('3','Three'),
+                new CheckboxElement('4','Four'),
+                new CheckboxElement('5','Five'))
+        );
+
+        $radio = new RadioElement('radio','Radio Element');
+        $radio->addButtonsArray(
+            array(
+                "1"=>"One",
+                "2"=>"Two",
+                "3"=>"Three",
+                "4"=>"Four",
+                "5"=>"Five"
+            )
+        );
 
         $submit = new SubmitElement('submit','Submit Element');
 
-        $this->addField($text, $requiredField, $password, $textarea, $submit);
+        $this->addField($text, $requiredText, $password, $textarea, $select, $hidden, $checkbox, $checkboxGroup, $radio, $submit);
     }
 }
