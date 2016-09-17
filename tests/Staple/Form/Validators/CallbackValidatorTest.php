5a2de2f2efe25c3061c0bdb34c224fd19179b7a8
hansheeling <pjhhs3>
2016-09-17 15:57:47 -0600

    Added unit test for CallbackValidator

---
+++ tests/Staple/Form/Validators/CallbackValidatorTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hans
+ * Date: 9/17/2016
+ * Time: 3:39 PM
+ */
+
+namespace Staple\Tests;
+
+
+use Staple\Form\Validate\CallbackValidator;
+
+class CallbackValidatorTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+	 * 
+	 * Verifies CallbackValidator instanciates
+	 */
+	public function testCreate()
+	{
+		$CallbackValidator = new CallbackValidator(function(){
+			return TRUE;
+		});
+		
+		$this->assertInstanceOf('Staple\Form\Validate\CallbackValidator', $CallbackValidator);
+	}
+
+	/**
+	 * @test
+	 *
+	 * Tests functionality of check method
+	 */
+	public function testCheck()
+	{
+		$CallbackValidator = new CallbackValidator(function($data){
+			return $data == TRUE ? TRUE : FALSE;
+		});
+
+		$this->assertTrue($CallbackValidator->check(TRUE));
+
+		$this->assertFalse($CallbackValidator->check(FALSE));
+	}
+}
+
