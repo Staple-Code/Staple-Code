7be6d51309e836305b12d5a6e5e446cfb00480fe
ironpilot <contact@ironpilot.net>
2017-05-11 17:22:40 -0600

    Add schema as option to Joins.

--- library/Staple/Query/Select.class.php
+++ library/Staple/Query/Select.class.php
@@ -594,11 +594,12 @@ class Select extends Query implements ISelectQuery
 	 * @param string $table
 	 * @param string $condition
 	 * @param string|null $alias
+	 * @param string|null $schema
 	 * @return $this
 	 */
-	public function leftJoin($table, $condition, $alias = NULL)
+	public function leftJoin($table, $condition, $alias = NULL, $schema = NULL)
 	{
-		$this->addJoin(Join::left($table, $condition,$alias));
+		$this->addJoin(Join::left($table, $condition, $alias, $schema));
 		return $this;
 	}
 
@@ -607,11 +608,12 @@ class Select extends Query implements ISelectQuery
 	 * @param string $table
 	 * @param string $condition
 	 * @param string|null $alias
+	 * @param string|null $schema
 	 * @return $this
 	 */
-	public function innerJoin($table, $condition, $alias = NULL)
+	public function innerJoin($table, $condition, $alias = NULL, $schema = NULL)
 	{
-		$this->addJoin(Join::inner($table, $condition,$alias));
+		$this->addJoin(Join::inner($table, $condition, $alias, $schema));
 		return $this;
 	}
 
